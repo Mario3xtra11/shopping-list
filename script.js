31d5dddff22db88a51b8d8e0593fc3d06145230b
MarioChandra06 <mariochandra064@gmail.com>
2023-06-29 21:12:11 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const itemform = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function onSubmit(e){
+    e.preventDefault();
+    const newItem = itemInput.value;
+
+    //Validate Input
+
+    if(newItem===''){
+        alert("Please add an item");
+        return;
+    }
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem));
+
+    const button = createButton("remove-item btn-link text-red");
+
+    li.appendChild(button);
+    itemList.appendChild(li);
+    itemInput.value = "";
+
+    function createButton(classes) {
+        const button = document.createElement('button');
+        button.className = classes;
+        const icon = createIcon("fa-solid fa-xmark");
+        button.appendChild(icon);
+        return button;
+    }
+
+    function createIcon(classes) {
+        const icon = document.createElement('i');
+        icon.className = classes;
+        return icon;
+    }
+;}
+
+
+itemform.addEventListener('submit', onSubmit);
